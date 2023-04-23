b57a6ba85ca698e97e0f78a8cbf0a5be34b9628f
PKC2001 <chillawarpranav@gmail.com>
2023-04-23 17:32:49 +0530

    added button style in globals, created search page, implemented useRouter hook

---
+++ src/pages/search.js
@@ -0,0 +1,33 @@
+import Header from '@/components/Header'
+import { useRouter } from 'next/router'
+import { format } from "date-fns";
+import React from 'react'
+
+const Search = () => {
+    const router = useRouter();
+    const { location, startDate, endDate, noOfGuests } = router.query;
+    const formatedStartDate =  format(new Date(startDate), "dd MMMM yy")
+    const formatedEndDate =  format(new Date(endDate), "dd MMMM yy")
+    const range = `${formatedStartDate} - ${formatedEndDate}`;
+
+  return (
+    <div>
+        <Header />
+        <main className='flex'>
+            <section className='flex-grow pt-4 px-6'>
+                <p className='text-xs'> 300+ stays [{range}] for {noOfGuests} guests</p>
+                <h1 className='text-3xl font-semibold mt-2 mb-6'>Stays in {location}</h1>
+                <div className='hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap'>
+                    <p className='button'>Cancellation Flexiblity</p>
+                    <p className='button'>Type of Place</p>
+                    <p className='button'>Price</p>
+                    <p className='button'>Rooms and Beds</p>
+                    <p className='button'>More filters</p>
+                </div>
+            </section>
+        </main>
+    </div>
+  )
+}
+
+export default Search
